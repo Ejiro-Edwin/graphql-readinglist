813601124321c58bbdba30b4d21d6cd71ae0dd07
Marcian Diamond <marciandmnd@yahoo.com>
2018-04-07 18:45:31 -0700

    Dummy data and resolve function implementation

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,15 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// dummy data
+
+var books = [
+  {name: 'Gone with the Wind', genre: 'Fantasy', id: '1'},
+  {name: 'Lord of the Rings', genre: 'Fantasy', id: '2'},
+  {name: 'Star Wars', genre: 'Sci Fi', id: '3'}
+];
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
@@ -9,4 +18,22 @@ const BookType = new GraphQLObjectType({
     name: {type: GraphQLString},
     genre: {type: GraphQLString}
   })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: {id: {type: GraphQLString}},
+      resolve(parent, args) {
+        // code to get data from db / other source
+        return _.find(books, {id: args.id})
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
