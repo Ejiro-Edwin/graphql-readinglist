37a9da94d42e200a44cacbe6419403da32d1f41d
Marcian Diamond <marciandmnd@yahoo.com>
2018-04-16 13:08:02 -0700

    Connect to MongoDB

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
+require('dotenv').config()
+
 const express = require('express');
+const mongoose = require('mongoose');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const app = express();
+mongoose.connect(process.env.MONGODB_URI);
 
+mongoose.connection.once('open', () => {
+  console.log('Connected to mlab!')
+});
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
